77cfff1b5a94129ebca86c6acc09b1b36224ea44
Andrew MacDonald <awm@alum.mit.edu>
2017-12-30 19:39:16 -0800

    Update indentation

--- yearly_problem.py
+++ yearly_problem.py
@@ -17,45 +17,45 @@ import itertools
 perm_seen = set()
 
 def groups_from_digits(digits):
-	if len(digits) == 1:
-		return [digits]
+  if len(digits) == 1:
+    return [digits]
 
-	last = digits[-1]
-	rest = groups_from_digits(digits[:-1])
+  last = digits[-1]
+  rest = groups_from_digits(digits[:-1])
 
-	groups = []
-	for g in rest:
-		g1 = g + (last,)
-		groups.append(g1)
-		if g[-1] != 0:
-			g2 = g[:-1] + (g[-1] * 10 + last,)
-			groups.append(g2)
+  groups = []
+  for g in rest:
+    g1 = g + (last,)
+    groups.append(g1)
+    if g[-1] != 0:
+      g2 = g[:-1] + (g[-1] * 10 + last,)
+      groups.append(g2)
 
-	return groups
+  return groups
 
 def solve_digit_tuple(digits):
-	groups = groups_from_digits(digits)
-	
-	print groups
+  groups = groups_from_digits(digits)
+  
+  print groups
 
 def solve(year):
-	digits = [int(d) for d in str(year)]
-	print digits
+  digits = [int(d) for d in str(year)]
+  print digits
 
-	for x in itertools.permutations(digits):
-		if x in perm_seen:
-			continue
-		perm_seen.add(x)
-		solve_digit_tuple(x)
+  for x in itertools.permutations(digits):
+    if x in perm_seen:
+      continue
+    perm_seen.add(x)
+    solve_digit_tuple(x)
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("year", type=int)
-	args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("year", type=int)
+  args = parser.parse_args()
 
-	print ("Solution for year {}:".format(args.year))
+  print ("Solution for year {}:".format(args.year))
 
-	solve(args.year)
+  solve(args.year)
 
 
 if __name__ == "__main__":
