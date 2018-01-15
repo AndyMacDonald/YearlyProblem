b1c22f04fac8095a592f79c59985f4b035cfb2ef
Andrew MacDonald <awm@alum.mit.edu>
2018-01-14 20:11:36 -0800

    Group digits and start tree construction

--- yearly-problem.elm
+++ yearly-problem.elm
@@ -25,6 +25,10 @@ type alias Precedence = Dict.Dict Op Int
 
 type alias Associative = Dict.Dict Op Bool
 
+type OpTree
+  = Leaf Int
+  | Node Op Int Int
+
 type alias Model =
   { content : String
   }
@@ -58,9 +62,15 @@ view model =
   div []
     [ input [ type_ "number", placeholder "Year", onInput Change ] []
     , div [] 
-          [ ol [] (List.map (\s -> li [] [text s]) (uniquePermutations model.content))
+          [ ol [] (List.map (\s -> li [] [text s]) (digitGroups model.content))
           ]
     ]
+
+digitGroups : String -> List String
+digitGroups s =
+  uniquePermutations s |>
+    List.concatMap groupDigits |>
+      List.filter (not << hasLeadingZeros)
 
 -- Deduplicate a set of permuations. Not guaranteed to preserve order
 uniquePermutations : String -> List String
@@ -112,3 +122,65 @@ swap s l r =
                  String.slice (l+1) r s, 
                  String.slice l (l+1) s, 
                  String.slice (r+1) (String.length s) s]
+
+-- For a numerical string, return a list containing all the ways
+-- the string can be split. Each entry is a string of comma separated groups
+groupDigits : String -> List String
+groupDigits s =
+  if String.length s == 1 then
+    s :: []
+  else
+    case String.uncons s of
+      Nothing -> []
+      Just (first, rest) ->
+        let
+          restGroups = groupDigits rest
+          g1 = List.map (\s -> String.cons first s) restGroups
+          g2 = List.map (\s -> String.cons ',' s |> String.cons first) restGroups
+        in
+          List.append g1 g2
+
+hasLeadingZeros : String -> Bool
+hasLeadingZeros s =
+  String.split "," s |>
+    List.any (\x ->
+      case String.uncons x of
+        Nothing -> False
+        Just (c, s) -> c == '0' && String.length s > 0
+      )
+
+treesFromGroups : List String -> List OpTree
+treesFromGroups groups =
+  List.map numsFromString groups |>  -- List List (Result String Int)
+    List.filter noStringErrors |>
+      List.concatMap (treesFromDigits << resultToInt) -- List Optree
+
+treesFromDigits : List Int -> List OpTree
+treesFromDigits nums =
+  case nums of
+    [] -> [] -- should never happen
+    h :: [] -> List.singleton (Leaf h)
+    _ :: t -> []
+
+    --(naiveTree s) :: []
+
+naiveTree : List Int -> OpTree
+naiveTree nums =
+  Leaf 0
+
+numsFromString : String -> List (Result String Int)
+numsFromString s =
+  String.split "," s |>
+    List.map String.toInt
+
+noStringErrors : List (Result String Int) -> Bool
+noStringErrors =
+  not << List.any (\n -> case n of
+                          Ok _ -> False
+                          Err _ -> True)
+
+resultToInt : List (Result String Int) -> List Int
+resultToInt =
+  List.map (\n -> case n of
+                    Ok i -> i
+                    Err _ -> 0) -- Filter out errors before so this is never reached
